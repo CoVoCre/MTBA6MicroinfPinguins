95b2c57e6393acb1c6922eefda6a6e82c33591a0
Théophane Mayaud <theophanemayaud@gmail.com>
2020-04-07 10:09:16 +0200

    added printf function, it builds, now will work on reading from serial

--- eclipse/comms.c
+++ eclipse/comms.c
@@ -10,17 +10,16 @@
  */
 
 #include <comms.h>
-#include <chprintf.h>
-
-#define USB_PORT SDU1
-#define UART_PORT SD3
+#include <usbcfg.h>
 
 //chprintf((BaseSequentialStream *)&SD3, "%Ax=%-7d Ay=%-7d Az=%-7d Gx=%-7d Gy=%-7d Gz=%-7d\r\n",
 //                imu_values.acc_raw[X_AXIS], imu_values.acc_raw[Y_AXIS], imu_values.acc_raw[Z_AXIS],
 //                imu_values.gyro_raw[X_AXIS], imu_values.gyro_raw[Y_AXIS], imu_values.gyro_raw[Z_AXIS]);
 
-
-static void comms_start(void)
+/**
+ * @brief   starts all communication things
+ */
+void comms_start(void)
 {
 	static SerialConfig ser_cfg = {
 	    115200,
@@ -29,12 +28,66 @@ static void comms_start(void)
 	    0,
 	};
 
-
-	//starts the serial communication
-	serial_start();
+	//starts the serial communication on UART
+	sdStart(&UART_PORT, &ser_cfg);
 	//start the USB communication
 	usb_start();
-
-	sdStart(&UART_PORT, &ser_cfg);
-	sdStart(&USB_PORT, &ser_cf);
 }
+
+/**
+ * @brief   use same function as chprintf() from chibios for sending information : System formatted output function.
+ * @details This function implements a minimal @p printf() like functionality
+ *          with output on a @p BaseSequentialStream.
+ *          The general parameters format is: %[-][width|*][.precision|*][l|L]p.
+ *          The following parameter types (p) are supported:
+ *          - <b>x</b> hexadecimal integer.
+ *          - <b>X</b> hexadecimal long.
+ *          - <b>o</b> octal integer.
+ *          - <b>O</b> octal long.
+ *          - <b>d</b> decimal signed integer.
+ *          - <b>D</b> decimal signed long.
+ *          - <b>u</b> decimal unsigned integer.
+ *          - <b>U</b> decimal unsigned long.
+ *          - <b>c</b> character.
+ *          - <b>s</b> string.
+ *
+ * @param[in] chp       pointer to a @p BaseSequentialStream implementing object
+ * @param[in] fmt       formatting string
+ *
+ *@return              The number of bytes that would have been
+ *                      written to @p chp if no stream error occurs
+ */
+int comms_printf(BaseSequentialStream *chp, const char *fmt, ...) {
+	va_list ap;
+	int formatted_bytes;
+
+	va_start(ap, fmt);
+	formatted_bytes = chvprintf(chp, fmt, ap);
+	va_end(ap);
+
+	return formatted_bytes;
+}
+
+/**
+ * @brief   read from  USB_PORT or UART_PORT
+ * @details This function implements a minimal @p printf() like functionality
+ *          with output on a @p BaseSequentialStream.
+ *          The general parameters format is: %[-][width|*][.precision|*][l|L]p.
+ *          The following parameter types (p) are supported:
+ *          - <b>x</b> hexadecimal integer.
+ *          - <b>X</b> hexadecimal long.
+ *          - <b>o</b> octal integer.
+ *          - <b>O</b> octal long.
+ *          - <b>d</b> decimal signed integer.
+ *          - <b>D</b> decimal signed long.
+ *          - <b>u</b> decimal unsigned integer.
+ *          - <b>U</b> decimal unsigned long.
+ *          - <b>c</b> character.
+ *          - <b>s</b> string.
+ *
+ * @param[in] chp       pointer to a @p BaseSequentialStream implementing object
+ * @param[in] fmt       formatting string
+ *
+ *@return              The number of bytes that would have been
+ *                      written to @p chp if no stream error occurs
+ */
--- eclipse/comms.h
+++ eclipse/comms.h
@@ -11,8 +11,42 @@
 #ifndef COMMS_H_
 #define COMMS_H_
 
+#include <ch.h>
+#include <hal.h>
+#include <chprintf.h>
+
+#define USB_PORT SDU1
+#define UART_PORT SD3
+
+/**
+ * @brief   starts all communication things
+ */
+void comms_start(void);
 
 
-
+/**
+ * @brief   use same function as chprintf() from chibios for sending information : System formatted output function.
+ * @details This function implements a minimal @p printf() like functionality
+ *          with output on a @p BaseSequentialStream.
+ *          The general parameters format is: %[-][width|*][.precision|*][l|L]p.
+ *          The following parameter types (p) are supported:
+ *          - <b>x</b> hexadecimal integer.
+ *          - <b>X</b> hexadecimal long.
+ *          - <b>o</b> octal integer.
+ *          - <b>O</b> octal long.
+ *          - <b>d</b> decimal signed integer.
+ *          - <b>D</b> decimal signed long.
+ *          - <b>u</b> decimal unsigned integer.
+ *          - <b>U</b> decimal unsigned long.
+ *          - <b>c</b> character.
+ *          - <b>s</b> string.
+ *
+ * @param[in] chp       pointer to a @p BaseSequentialStream implementing object
+ * @param[in] fmt       formatting string
+ *
+ *@return              The number of bytes that would have been
+ *                      written to @p chp if no stream error occurs
+ */
+int comms_printf(BaseSequentialStream *chp, const char *fmt, ...);
 
 #endif /* COMMS_H_ */
