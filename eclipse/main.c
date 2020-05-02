d358b06faeab1062094949a071241029166f8298
Théophane Mayaud <theophanemayaud@gmail.com>
2020-05-02 18:35:09 +0200

    reworded smashing comoments in main

--- eclipse/main.c
+++ eclipse/main.c
@@ -315,7 +315,7 @@ void moveTowardsTarget(Destination *destination)
 }
 
 /*===========================================================================*/
-/* Something to protect agains something...                                  */
+/* Code to protect against smashing
 /*===========================================================================*/
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
